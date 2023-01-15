7b99b3b48392486e5d7c96e4b02b90d3bf05be34
Valentina Carpentiero <carpentierovalentina8@gmail.com>
2023-01-15 16:30:32 +0100

    Exercise - SQL-select

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+import java.sql.*;
+import java.util.ArrayList;
+
+public class Main {
+    static final String URL = "jdbc:mysql://localhost:3306/newdb";
+    static final String USER = "developer";
+    static final String PASSWORD = "userpasw";
+
+
+    public static void main(String[] args) {
+
+        ArrayList<String> surnames = new ArrayList<>();
+
+        try (Connection conn = DriverManager.getConnection(URL, USER, PASSWORD);
+        PreparedStatement preparedStatement = conn.prepareStatement("SELECT last_name, first_name FROM students")) {
+
+            ResultSet result = preparedStatement.executeQuery();
+
+            while (result.next()) {
+                System.out.println("Extracted name: " + result.getString("first_name"));
+                surnames.add(result.getString("last_name"));
+            }
+
+            System.out.println("All Surnames: " + surnames);
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
